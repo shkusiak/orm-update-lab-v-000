36283f79a07e5008b2ba4e63b976bc6dc3d2b32e
Shea Kusiak <shkusiak@gmail.com>
2018-06-28 19:27:23 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,9 +42,9 @@ class Student
   end
 
   def self.create(name:, grade:)
-    song = Song.new(name, grade)
-    song.save
-    song
+    student = Student.new(name, grade)
+    student.save
+    student
   end
 
   def self.new_from_db
