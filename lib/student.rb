e0445dc94e143e5d349757be1a5a84cfb8ceed20
Shea Kusiak <shkusiak@gmail.com>
2018-06-28 19:03:27 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,19 +9,36 @@ class Student
     @id = id
     @album = album
   end
-  
+
   def self.create_table
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY,
-        name TEXT, 
+        name TEXT,
         grade TEXT
       )
     SQL
 
     DB[:conn].execute(sql)
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  end
 
+  def self.drop_table
+    "DROP TABLE IF EXISTS students";
+  end
+
+  def save
+
+  end
+
+  def self.create
+  end
+  def self.new_from_db
+  end
+  def self.find_by_name
+  end
+  def update
+  end
+
+  end
 
 end
