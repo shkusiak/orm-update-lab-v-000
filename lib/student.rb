3536e4dc4c4bc3d8d4aabcdb55bb393d621d94bc
Shea Kusiak <shkusiak@gmail.com>
2018-06-28 19:31:57 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,11 +48,10 @@ class Student
   end
 
   def self.new_from_db(row)
-    student = self.new
-    student.id = row[0]
-    student.name = row[1]
-    student.grade = row[2]
-    student
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
   end
 
   def self.find_by_name(name)
