dd7db500e59dea457ef9774bb444a33872f79a93
Shea Kusiak <shkusiak@gmail.com>
2018-06-28 19:36:52 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ require_relative "../config/environment.rb"
 class Student
   attr_accessor :name, :grade, :id
 
-  def initialize(name, grade, id=nil)
+  def initialize(id=nil, name, grade)
     @name = name
     @id = id
     @grade = grade
